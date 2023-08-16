362fb7df36350b17e7e40ae49bd86a0774c5cb0d
Wellington <wellington.map@hotmail.com>
2023-08-16 10:21:41 -0300

    chore: add environment vars config

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,11 @@
 import { app } from "./app";
+import { env } from "./env";
 
 app
   .listen({
     host: "0.0.0.0",
-    port: 3333,
+    port: env.PORT,
   })
   .then(() => {
-    console.log("HTTP Server Running!");
+    console.log(`HTTP Server Running on Port: ${env.PORT}`);
   });
